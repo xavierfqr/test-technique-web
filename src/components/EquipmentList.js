9e8b19567fa6f04f29382e59736472e9ade57314
Xavier <facqueur.xavier@gmail.com>
2021-12-20 13:17:21 +0100

    Add button change page

--- src/components/EquipmentList.js
+++ src/components/EquipmentList.js
@@ -6,14 +6,23 @@ import styles from './EquipmentList.module.css'
 
 function EquipmentList() {
 
+    const [pageCount, setPageCount] = React.useState(0);
     const {equipments, filterOption, filterValue} = useSelector(state => state.equipmentsReducer)
+
+    React.useEffect(() => {
+        setPageCount(0)
+    }, [filterOption, filterValue])
+
+    React.useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pageCount])
     
     const equipmentKeys = Object.keys(equipments);
 
     const equipmentList = equipmentKeys.map(key => {
         const {name, domain, nbFaults, photo, brand, niveau} = equipments[key]
         let shouldDisplay = false; 
-        switch(filterOption) {
+        switch (filterOption) {
             case 'name':
                 shouldDisplay = name.toLowerCase().includes(filterValue.toLowerCase());
                 break;
@@ -32,11 +41,15 @@ function EquipmentList() {
         if (!shouldDisplay) return null;
         
         return <EquipmentCard key={key} id={key} name={name} domain={domain} nbFaults={nbFaults} photo={photo}></EquipmentCard>
-    })
+    }).filter(elem => elem !== null).slice(pageCount * 10, (pageCount + 1) * 10);
 
     return (
         <div className={styles.container}>
             {equipmentList}
+            <div className={styles.pageButtons}>
+                <button disabled={pageCount === 0} onClick={() => setPageCount(count => count - 1)}>Précédent</button>
+                <button disabled={equipmentList.length < 10} onClick={() => setPageCount(count => count + 1)}>Suivant</button>
+            </div>
         </div>
     )
 }
